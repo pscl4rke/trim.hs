cccd6b17288ad97943f0f650b4dd1b188ccf0151
P. S. Clarke <trim.hs@pscl4rke.net>
2020-08-04 08:15:24 +0100

    Trim to 20 characters every time

--- Main.hs
+++ Main.hs
@@ -3,10 +3,7 @@
 -- NB: The output will always end with a new line, even if the input didn't
 
 
-trim :: String -> String
-trim xs = xs
-
-
 main :: IO ()
 main = do
-    interact $ unlines . (map trim) . lines
+    let width = 20
+    interact $ unlines . (map (take width)) . lines
