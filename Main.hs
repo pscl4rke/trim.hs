fe3a6871c274c55842bdfb7992b53d5cc2fb21f6
P. S. Clarke <trim.hs@pscl4rke.net>
2020-08-04 08:47:35 +0100

    Optional width on cmdline

--- Main.hs
+++ Main.hs
@@ -1,9 +1,17 @@
 
 
+import System.Environment (getArgs)
+
+
 -- NB: The output will always end with a new line, even if the input didn't
 
 
 main :: IO ()
 main = do
-    let width = 20
+    let default_width = 20 -- FIXME look up current terminal size
+    cmdLineArgs <- getArgs
+    let width = case cmdLineArgs of
+                        [] -> default_width
+                        [width_str] -> read width_str -- FIXME handle unparseable
+                        _ -> error "too many args"
     interact $ unlines . (map (take width)) . lines
